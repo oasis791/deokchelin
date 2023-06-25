17e5c98d6e3856d1e59c9412d2345b9dcdc68391
oasis791 <khwi791@naver.com>
2023-06-26 03:30:13 +0900

    initial Commit

---
+++ src/main/java/kimhyunwook/deokchelinserver/DeokchelinServerApplication.java
@@ -0,0 +1,13 @@
+package kimhyunwook.deokchelinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DeokchelinServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DeokchelinServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kimhyunwook/deokchelinserver/DeokchelinServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kimhyunwook.deokchelinserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DeokchelinServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
